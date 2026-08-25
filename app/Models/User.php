98cecb053d46938a1a77b40ad2c6f3e5aee443de
jawad-farooqi <234539674+jawad-farooqi@users.noreply.github.com>
2026-08-26 02:19:29 +0500

    add users

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,6 +17,18 @@ class User extends Authenticatable
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
 
+     protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'is_active',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
     /**
      * Get the attributes that should be cast.
      *
@@ -27,6 +39,7 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'is_active' => 'boolean',
         ];
     }
 }
