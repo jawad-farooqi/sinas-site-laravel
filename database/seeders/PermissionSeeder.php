6f1c706adc7ae169979ba65b7f7160860d1b8c8a
jawad-farooqi <234539674+jawad-farooqi@users.noreply.github.com>
2026-08-29 16:56:05 +0500

    add PermissionSeeder

---
+++ database/seeders/PermissionSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+
+class PermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        Permission::firstOrCreate([
+            'name' => 'delete users',
+            'guard_name' => 'web',
+        ]);
+    }
+}
