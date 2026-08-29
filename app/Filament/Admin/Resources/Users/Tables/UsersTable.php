d15e48b4818aab24dc72ead5531cb4d6aac26a6a
jawad-farooqi <234539674+jawad-farooqi@users.noreply.github.com>
2026-08-29 11:12:12 +0500

    display roles in users module

--- app/Filament/Admin/Resources/Users/Tables/UsersTable.php
+++ app/Filament/Admin/Resources/Users/Tables/UsersTable.php
@@ -22,9 +22,10 @@ class UsersTable
                 TextColumn::make('email')
                     ->label('Email address')
                     ->searchable(),
-                TextColumn::make('email_verified_at')
-                    ->dateTime()
-                    ->sortable(),
+                TextColumn::make('roles.name') 
+                    ->label('Role') 
+                    ->badge() 
+                    ->searchable(),
                 IconColumn::make('is_active')
                     ->boolean(),
                 TextColumn::make('created_at')
