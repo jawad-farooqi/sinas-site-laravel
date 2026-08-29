1e21a5f2961ffcf337f1554925de6913b9ac6562
jawad-farooqi <234539674+jawad-farooqi@users.noreply.github.com>
2026-08-30 01:21:32 +0500

    redirect the user to ListUsers page after editing record

--- app/Filament/Admin/Resources/Users/Pages/EditUser.php
+++ app/Filament/Admin/Resources/Users/Pages/EditUser.php
@@ -11,6 +11,11 @@ class EditUser extends EditRecord
 {
     protected static string $resource = UserResource::class;
 
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+
     private ?string $selectedRole = null;
 
     protected function getHeaderActions(): array
