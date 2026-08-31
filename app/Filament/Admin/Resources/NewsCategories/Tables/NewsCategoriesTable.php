8c554d0e417af9f677818e9efd9e1d63bab5503a
jawad-farooqi <234539674+jawad-farooqi@users.noreply.github.com>
2026-08-31 16:16:20 +0500

    remove bulk actions option from news category form

--- app/Filament/Admin/Resources/NewsCategories/Tables/NewsCategoriesTable.php
+++ app/Filament/Admin/Resources/NewsCategories/Tables/NewsCategoriesTable.php
@@ -41,9 +41,9 @@ class NewsCategoriesTable
                 EditAction::make(),
             ])
             ->toolbarActions([
-                BulkActionGroup::make([
-                    DeleteBulkAction::make(),
-                ]),
+                // BulkActionGroup::make([
+                //     DeleteBulkAction::make(),
+                // ]),
             ]);
     }
 }
